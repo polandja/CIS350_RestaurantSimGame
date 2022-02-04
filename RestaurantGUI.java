285587aca32211c12f7ca49e5447daf3950e6910
Hanna Halstead <halsteha@mail.gvsu.edu>
2022-02-03 19:13:17 -0500

    Initial GUI Code

---
+++ RestaurantGUI.java
@@ -0,0 +1,11 @@
+//import javax.swing.*;
+
+//public class RestaurantGUI {
+    //public static void main(String[] args) {
+        //JMenu fileMenu;
+        //JMenuItem quitItem;
+        //JMenuBar menus;
+
+
+    //}
+//}
