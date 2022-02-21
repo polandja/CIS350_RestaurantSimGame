5471eca400cad9aba58869b45299ff49d52d10bd
Hanna Halstead <halsteha@mail.gvsu.edu>
2022-02-21 10:35:12 -0500

    Added Recipe Book and more buttons

--- RestaurantPanel.java
+++ RestaurantPanel.java
@@ -6,12 +6,28 @@ import java.awt.event.ActionListener;
 //Restaurant Panel Class
 public class RestaurantPanel extends JPanel {
     JButton bunButton;
+    JButton pattyButton;
+    JButton cheeseButton;
+    JButton recipeBook;
 
     public RestaurantPanel() {
-        bunButton = new JButton("Hamburger Bun");
+        bunButton = new JButton("Bun");
+        bunButton.setAlignmentY(1f);
         add(bunButton);
 
+        pattyButton = new JButton("Patty");
+        add(pattyButton);
+
+        cheeseButton = new JButton("Cheese");
+        add(cheeseButton);
+
+        recipeBook = new JButton("Recipe Book");
+        add(recipeBook);
+
         bunButton.addActionListener(new ButtonListener());
+        pattyButton.addActionListener(new ButtonListener());
+        cheeseButton.addActionListener(new ButtonListener());
+        recipeBook.addActionListener(new ButtonListener());
     }
 
     private class ButtonListener implements ActionListener {
@@ -19,6 +35,16 @@ public class RestaurantPanel extends JPanel {
             if (event.getSource() == bunButton) {
                 System.out.println("Hamburger Bun");
             }
+            if (event.getSource() == pattyButton) {
+                System.out.println("Beef Patty");
+            }
+            if (event.getSource() == cheeseButton) {
+                System.out.println("Cheddar Cheese");
+            }
+            if (event.getSource() == recipeBook) {
+                RecipeBook recipeBookWindow = new RecipeBook();
+            }
+
         }
     }
 }
