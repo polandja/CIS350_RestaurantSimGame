72f7eee8561a1c950132921719e934fe7bcbc282
Hanna Halstead <halsteha@mail.gvsu.edu>
2022-02-22 16:16:17 -0500

    Added orderInProgress class to RestaurantPanel

--- RestaurantPanel.java
+++ RestaurantPanel.java
@@ -5,10 +5,13 @@ import java.awt.event.ActionListener;
 
 //Restaurant Panel Class
 public class RestaurantPanel extends JPanel {
+    private orderInProgress currentOrder;
+    
     JButton bunButton;
     JButton pattyButton;
     JButton cheeseButton;
     JButton recipeBook;
+    JButton clearButton;
 
     public RestaurantPanel() {
         bunButton = new JButton("Bun");
@@ -24,10 +27,14 @@ public class RestaurantPanel extends JPanel {
         recipeBook = new JButton("Recipe Book");
         add(recipeBook);
 
+        clearButton = new JButton("Clear");
+        add(clearButton);
+
         bunButton.addActionListener(new ButtonListener());
         pattyButton.addActionListener(new ButtonListener());
         cheeseButton.addActionListener(new ButtonListener());
         recipeBook.addActionListener(new ButtonListener());
+        clearButton.addActionListener(new ButtonListener());
     }
 
     private class ButtonListener implements ActionListener {
@@ -43,6 +50,9 @@ public class RestaurantPanel extends JPanel {
             }
             if (event.getSource() == recipeBook) {
                 RecipeBook recipeBookWindow = new RecipeBook();
+            }
+            if (event.getSource() == clearButton) {
+                currentOrder.trash();
             }
 
         }
