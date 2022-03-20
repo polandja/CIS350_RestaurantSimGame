b95283d2039a696e620c187a6c817b8ffc4d304b
polandja <zeta.poland@gmail.com>
2022-03-20 17:44:13 -0400

    full menu intialized and round variable

--- RestaurantPanel.java
+++ RestaurantPanel.java
@@ -16,22 +16,56 @@ import java.awt.event.ActionListener;
  ************************************************************/
 public class RestaurantPanel extends JPanel {
     
+    //New Round Counter
+    public int round = 1; 
+    
+
     // New OrderInProgress playerOrder
     private OrderInProgress playerOrder = new OrderInProgress();
     
     // New Order custOrder
     private Order custOrder = new Order();
 
+
     // Initializing Hamburger
     ArrayList<String> burgerRec = new ArrayList<String>(Arrays.asList("Bun", "Patty", "Bun"));
     MenuItem burger = new MenuItem("Hamburger", 10, burgerRec);
 
-    // Initializing Cheeseburger
+    // Initializing CheeseBurger
     ArrayList<String> cheeseBurgerRec = new ArrayList<String>(Arrays.asList("Bun", "Patty", "Cheese", "Bun"));
-    MenuItem cheeseBurger = new MenuItem("Cheeseburger", 12, cheeseBurgerRec);
+    MenuItem cheeseBurger = new MenuItem("CheeseBurger", 11, cheeseBurgerRec);
+
+    // Initializing AmericanBurger
+    ArrayList<String> americanBurgerRec = new ArrayList<String>(Arrays.asList("Bun", "Patty", "Cheese", "Lettuce", "Tomato", "Bun"));
+    MenuItem americanBurger = new MenuItem("AmericanBurger", 15, americanBurgerRec);
+   
+    // Initializing QuarterPounder
+    ArrayList<String> quarterPounderRec = new ArrayList<String>(Arrays.asList("Bun", "Patty", "Onion", "Bun", "Patty", "Cheese", "Bun"));
+    MenuItem quarterPounder = new MenuItem("QuarterPounder", 18, quarterPounderRec);
+    
+    // Initializing AmericanBurger
+    ArrayList<String> veggieBurgerRec = new ArrayList<String>(Arrays.asList("Bun", "Cheese", "Lettuce", "Tomato", "Onion", "Bun"));
+    MenuItem veggieBurger = new MenuItem("VeggieBurger", 15, veggieBurgerRec);
+
+    // Initializing Salad
+    ArrayList<String> saladRec = new ArrayList<String>(Arrays.asList("Lettuce", "Tomato", "Onion"));
+    MenuItem salad = new MenuItem("Salad", 8, saladRec);
+
+    // Initializing Fries
+    ArrayList<String> friesRec = new ArrayList<String>(Arrays.asList("Fries"));
+    MenuItem fries = new MenuItem("Fries", 3, friesRec);
+
+    // Initializing LoadedFries
+    ArrayList<String> loadedFriesRec = new ArrayList<String>(Arrays.asList("Fries", "Patty", "Cheese"));
+    MenuItem loadedFries = new MenuItem("LoadedFries", 5, loadedFriesRec);
+
+    // Initializing IceCream
+    ArrayList<String> iceCreamRec = new ArrayList<String>(Arrays.asList("IceCream"));
+    MenuItem iceCream = new MenuItem("IceCream", 3, iceCreamRec);
 
     // Initializing the Menu
     ArrayList<MenuItem> menu = new ArrayList<MenuItem>(Arrays.asList(burger, cheeseBurger));
+
     
     // Button bunButton
     JButton bunButton;
