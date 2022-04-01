d0c75e5eb9281222f0f085e22bddec337e479a7c
Jacque <79173064+polandja@users.noreply.github.com>
2022-04-01 10:31:27 -0400

    class comment update

--- OrderInProgress.java
+++ OrderInProgress.java
@@ -10,7 +10,7 @@ import java.util.*;
  * 
  * 
  * @author Claire Grob, Hanna Halstead, and Jacqueline Poland
- * @version March 4, 2022
+ * @version April 18, 2022
  ************************************************************/
 public class OrderInProgress {
 
